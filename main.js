3b12798a398d8e8912d748400c47f7dc05298580
6206021621155@fitm.kmutnb.ac.th <6206021621155@fitm.kmutnb.ac.th>
2021-03-25 09:44:22 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+/*const product = 'Socks'
+*/
+const app = Vue.createApp({
+    data(){
+        return {
+            product : 'Boots',
+            description : 'A Boots a here '
+
+        }
+    }
+})
